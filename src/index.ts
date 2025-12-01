6a4b0c38d98d2e4ff566e0b10031061128edd937
Ben-Dess <benoit.dessaint2003@gmail.com>
2025-12-01 15:21:04 +0100

    update questions

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,6 @@ import { fileURLToPath } from 'url'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import bcrypt from "bcryptjs"
-import * as mongoDB from "mongodb";
-import * as dotenv from "dotenv";
 
 dotenv.config()
 
@@ -23,22 +21,6 @@ if (!mongoose.connection.readyState) {
   console.log('Connected to MongoDB')
 }
 
-export const collections: { Questions?: mongoDB.Collection } = {}
-
-export async function connectToDatabase () {
-
-   const client: mongoDB.MongoClient = new mongoDB.MongoClient(process.env.MONGODB_URI);
-           
-   await client.connect();
-       
-   const db: mongoDB.Db = client.db("Donjon");
-  
-   const gamesCollection: mongoDB.Collection = db.collection("Questions");
-
- collections.Questions = gamesCollection;
-      
-        console.log(`Successfully connected to database: ${db.databaseName} and collection: ${gamesCollection.collectionName}`);
-}
 // User schema
 const userSchema = new mongoose.Schema({
   id: { type: String, required: true, unique: true },
